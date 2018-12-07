f46dec7c567df13c41c8e7e35ad44ee04c92e116
Arthur <arthmelikyan@gmail.com>
2018-12-07 15:49:29 +0400

    edit files

--- convert.php
+++ convert.php
@@ -2,36 +2,44 @@
 
 // ! load the Mimetypes package , to check file extension from mime-type text
 require './vendor/autoload.php';
-$mimes = new \Mimey\MimeTypes; 
 
 // * file name where saved base64 text
-$pdf_base64 = "./base64text.txt";
+$file_base64 = "./base64text.txt";
 
+// finish...
+
+
+function base64_to_file($fileName){
+    
 // * open/read the file
-$pdf_base64_handler = fopen($pdf_base64,'r');
-$pdf_content = fread ($pdf_base64_handler,filesize($pdf_base64));
-fclose ($pdf_base64_handler);
+    $file_base64_handler = fopen($fileName, 'r');
+    $file_content = fread($file_base64_handler, filesize($fileName));
+    fclose($file_base64_handler);
 
-//* get file extension by mime type text
-function getMIMETYPE($base64string){
-    //* croping matching text. 
-    preg_match("/^data:(.*);base64/",$base64string, $match);
-    return $match[1]; //return mime-type. For example image/jpeg or application/pdf
-}
 // checking the file extension...
-$mime_type = '.'.$mimes->getExtension(getMIMETYPE($pdf_content));
-print($mime_type);
+    $mimes = new \Mimey\MimeTypes;
+    $mime_type = '.' . $mimes->getExtension(getMIMETYPE($file_content));
+    print($mime_type);
 /* ____________ */
 
-// crop the part of text where text is ex. "data:application/pdf;base64," or "image/jpeg" or etc
+// crop the part of text where text is ex. "data:application/file;base64," or "image/jpeg" or etc
 // and decoding it...
-$pdf_decoded = base64_decode (preg_replace("/^data:(.*);base64,/",'',$pdf_content));
+    $file_decoded = base64_decode(preg_replace("/^data:(.*);base64,/", '', $file_content));
 /* _______________ */
 
 // generate a file by decoded base64 text
-$pdf = fopen (time().'_generated_'.$mime_type ,'w');
+    $file = fopen(time() . '_generated_' . $mime_type, 'w');
 
-fwrite ($pdf,$pdf_decoded);
+    fwrite($file, $file_decoded);
 
-fclose ($pdf);
-// finish...
+    fclose($file);
+}
+//* get file extension by mime type text
+function getMIMETYPE($base64string){
+    //* croping matching text. 
+    preg_match("/^data:(.*);base64/", $base64string, $match);
+    return $match[1]; //return mime-type. For example image/jpeg or application/file
+}
+
+// CALL FUNCTION 
+base64_to_file($file_base64);
